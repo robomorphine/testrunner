b0794a3d29ced8bb83d980334db0f7a5f003808f
inazaruk <ievgenii.nazaruk@gmail.com>
2011-12-18 01:51:34 +0200

    Added DeleteAvdTask.

---
+++ src/host-testrunner-ant-lib/src/com/robomorphine/test/ant/DeleteAvdTask.java
@@ -0,0 +1,50 @@
+package com.robomorphine.test.ant;
+
+import com.android.sdklib.internal.avd.AvdInfo;
+import com.android.sdklib.internal.avd.AvdManager;
+import com.robomorphine.test.TestManager;
+
+import org.apache.tools.ant.BuildException;
+
+public class DeleteAvdTask extends BaseTask {
+    
+    private String mName;
+    
+    public void setName(String name) { 
+        mName = name;
+    }
+    
+    AvdInfo findAvdInfo(String name) {
+        AvdManager avdManager = getTestManager().getAvdManager();
+        for(AvdInfo info : avdManager.getAllAvds()) {
+            if(info.getName().equals(mName)) {
+                return info;
+            }
+        }        
+        return null;
+    }
+    
+    @Override
+    public void execute() throws BuildException {        
+        if(mName == null) {
+            error("Avd name is not specified.");
+        }
+        
+        TestManager testManager = getTestManager();
+        AvdManager avdManager = getTestManager().getAvdManager();
+        AvdInfo avdInfo = findAvdInfo(mName);
+        
+        if(avdInfo == null) {
+            warn("Avd \"%s\" was not found. Assuming success.", mName);
+        } else {
+            avdManager.deleteAvd(avdInfo, testManager.getSdkLogger());
+            avdInfo = findAvdInfo(mName);
+            if(avdInfo == null) {
+                info("Avd %s was successfully deleted.", mName);
+            } else {
+                error("Failed to delete \"%s\" avd.", mName);
+            } 
+        }
+    }
+    
+}
